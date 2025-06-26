4806e0bfb875c62692ae4aab7d9e41406ce51e4b
Waveus <wav3us@gmail.com>
2025-06-26 23:05:08 +0200

    new db v2

--- database/zmitacSchema.sql
+++ database/zmitacSchema.sql
@@ -3,12 +3,12 @@ CREATE TABLE users (
   name TEXT NOT NULL,
   surname TEXT NOT NULL,
   student_id TEXT,
-  is_admin BOOLEAN
+  is_admin BOOLEAN NOT NULL
 );
 
 CREATE TABLE subjects (
   subject_id TEXT NOT NULL PRIMARY KEY,
-  subject_name TEXT
+  subject_name TEXT NOT NULL
 );
 
 CREATE TABLE roles (
@@ -45,21 +45,21 @@ CREATE TABLE assignments (
   assignment_id TEXT NOT NULL PRIMARY KEY,
   subject_id TEXT NOT NULL,
   title TEXT NOT NULL,
-  description TEXT,
-  accepted_mime_types TEXT, -- Store JSON or comma-separated values
+  description TEXT NOT NULL,
+  accepted_mime_types TEXT NOT NULL, -- Store JSON or comma-separated values
   FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
 );
 
 CREATE TABLE solutions (
   solution_id TEXT NOT NULL PRIMARY KEY,
   grade DECIMAL(3,2),
-  submission_date TIMESTAMP,
-  solution_data BLOB,
+  submission_date TIMESTAMP NOT NULL,
+  solution_data BLOB NOT NULL,
   reviewed_by TEXT,
   review_comment TEXT,
   review_date TIMESTAMP,
-  mime_type TEXT,
-  assignment_id TEXT,
+  mime_type TEXT NOT NULL,
+  assignment_id TEXT NOT NULL,
   FOREIGN KEY (reviewed_by) REFERENCES users(user_id),
   FOREIGN KEY (assignment_id) REFERENCES assignments(assignment_id)
 );
